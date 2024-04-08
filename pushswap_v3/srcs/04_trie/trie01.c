537e5fa547e6337e0e5dbc61d78486d7657aaece
CABARBAYE Adrien <cabarbaye.adrien@gmail.com>
2024-04-08 14:12:59 +0200

    Gestion des trie specifique ok

--- pushswap_v3/srcs/04_trie/trie01.c
+++ pushswap_v3/srcs/04_trie/trie01.c
@@ -23,10 +23,7 @@ void	step00(t_stack **stack_a, t_stack **stack_b)
 		trie_deux(stack_a, stack_b);
 	else if (size == 3)
 		trie_trois(stack_a, stack_b);
-	// else if (size == 4)
-	// 	trie_quatre(stack_a, stack_b);
-	// else if (size == 5)
-	// 	trie_cinq(stack_a, stack_b);
+	main_index(stack_a, stack_b);
 	return ;
 }
 
@@ -39,17 +36,13 @@ void	trie_deux(t_stack **stack_a, t_stack **stack_b)
 
 void	trie_trois(t_stack **stack_a, t_stack **stack_b)
 {
+	t_stack    *highest_node;
+
+	highest_node = lf_big_node(*stack_a);
+	if (*stack_a == highest_node)
+		ra(stack_a, stack_b);
+	else if ((*stack_a)->next == highest_node)
+		rra(stack_a, stack_b);
 	if ((*stack_a)->value > (*stack_a)->next->value)
 		sa(stack_a, stack_b);
-	if ((*stack_a)->value > (*stack_a)->next->next->value)
-	{
-		ra(stack_a, stack_b);
-	}
-	if ((*stack_a)->next->value > (*stack_a)->next->next->value)
-	{
-		rra(stack_a, stack_b);
-		sa(stack_a, stack_b);
-		ra(stack_a, stack_b);
-	}
-	return ;
-}
+}	
